8e2ffd197916b25a2de292916cd313c94a477a96
FusionSid <siddheshadsv@icloud.com>
2023-10-13 18:52:28 +1300

    refac: use the useClock hook in the code hook

--- core/helpers/useAccountsCode.tsx
+++ core/helpers/useAccountsCode.tsx
@@ -1,8 +1,8 @@
 import totp from "totp-generator";
 import { useState, useEffect } from "react";
 
-import { Accounts } from "../helpers/types";
-import { AccountsWithCode } from "../helpers/types";
+import useClock from "./useClock";
+import { Accounts, AccountsWithCode } from "../helpers/types";
 
 function useAccountsCode({ accounts }: { accounts: Accounts }): {
     accountsWithCode: AccountsWithCode;
@@ -10,18 +10,10 @@ function useAccountsCode({ accounts }: { accounts: Accounts }): {
     const [accountsWithCode, setAccountsWithCode] = useState<AccountsWithCode>(
         []
     );
+    const timeLeft = useClock();
 
     useEffect(() => {
-        setAccountsWithCode(
-            accounts.map((account) => {
-                return {
-                    ...account,
-                    code: totp(account.secret),
-                };
-            })
-        );
-
-        const loopUpdateCode = () => {
+        if (timeLeft === 30) {
             setAccountsWithCode(
                 accounts.map((account) => {
                     return {
@@ -30,26 +22,8 @@ function useAccountsCode({ accounts }: { accounts: Accounts }): {
                     };
                 })
             );
-        };
-
-        // run function once
-        loopUpdateCode();
-
-        // calculate how long current code is valid
-        const msTillNextCode =
-            (30 - (Math.round(Number(new Date()) / 1000) % 30)) * 1000;
-
-        let updateOTPInteval: NodeJS.Timeout;
-
-        // after current code is invalid create a loop that updates every 30 sec
-        setTimeout(() => {
-            updateOTPInteval = setInterval(loopUpdateCode, 30_000);
-        }, msTillNextCode);
-
-        return () => {
-            clearInterval(updateOTPInteval);
-        };
-    }, []);
+        }
+    }, [timeLeft]);
 
     return { accountsWithCode };
 }
