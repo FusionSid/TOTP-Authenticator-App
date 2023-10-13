71561463be394cbacd6ddf86501ff469b41e58e6
FusionSid <siddheshadsv@icloud.com>
2023-10-13 18:52:39 +1300

    feat: show the time left till next code

--- core/components/accounts.tsx
+++ core/components/accounts.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { View, Text, ScrollView, SafeAreaView } from "react-native";
 
+import useClock from "../helpers/useClock";
 import { Accounts } from "../helpers/types";
 import { AccountWithCode } from "../helpers/types";
 import useAccountsCode from "../helpers/useAccountsCode";
 
-const AccountItem = ({ account }: { account: AccountWithCode }) => {
+const AccountItem = ({
+    account,
+    timeLeft,
+}: {
+    account: AccountWithCode;
+    timeLeft: number;
+}) => {
     return (
-        <View>
-            <Text>{account.name}</Text>
-            <Text>{account.code}</Text>
+        <View className="flex-row justify-between w-full p-5 bg-blue-500">
+            <Text className="text-white text-xl">{account.name}</Text>
+            <Text className="text-white text-xl">{account.code}</Text>
+            <Text className="text-white text-xl">{timeLeft}</Text>
         </View>
     );
 };
@@ -18,12 +26,19 @@ const AccountsList = ({ accounts }: { accounts: Accounts }) => {
     const { accountsWithCode } = useAccountsCode({
         accounts,
     });
+    const timeLeft = useClock();
 
     return (
         <SafeAreaView>
             <ScrollView>
                 {accountsWithCode.map((account: AccountWithCode) => {
-                    return <AccountItem key={account.id} account={account} />;
+                    return (
+                        <AccountItem
+                            key={account.id}
+                            account={account}
+                            timeLeft={timeLeft}
+                        />
+                    );
                 })}
             </ScrollView>
         </SafeAreaView>
